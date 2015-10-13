dfcc2fd831303f02e02c22ba40f5cc86e450cfd9
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-10-13 06:10:59 +0200

    dev+

--- lib/MarpaX/RFC/RFC3987.pm
+++ lib/MarpaX/RFC/RFC3987.pm
@@ -9,39 +9,77 @@ package MarpaX::RFC::RFC3987;
 
 # AUTHORITY
 
+use Carp qw/croak/;
 use Class::Load qw/load_class/;
 use Encode qw/decode/;
 use MarpaX::RFC::RFC3987::_common;
 use MarpaX::RFC::RFC3987::_generic;
+use Scalar::Does;
 use Try::Tiny;
+use Types::Standard -all;
+use Types::Encodings qw/Bytes/;
 
 #
 # Writen like this because I want to control the class
 #
+my $has_recognized_scheme = 0;
+
 sub new {
   my $class = shift;
-  my $input = ($#_ <= 0) ? (shift || '') : decode(shift, shift, @_);
+
+  my $input = '';
+  if (@_) {
+    $input = shift;
+    if (ArrayRef->check($input)) {
+      croak 'Referenced array must have a least two elements'   if ($#{$input} < 1);
+
+      my $encoding = shift(@{$input});
+      croak 'Referenced array must have a string in first element' if (! Str->check($encoding));
+
+      my $bytes = shift(@{$input});
+      croak 'Referenced array must have bytes in second element' if (! Bytes->check($bytes));
+
+      $input = decode($encoding, $bytes, @{$input});
+    } else {
+      $input = "$input";
+    }
+  }
+  # Copy from URI:
+  # Get rid of potential wrapping
+  $input =~ s/^<(?:URL:)?(.*)>$/$1/;
+  $input =~ s/^"(.*)"$/$1/;
+  $input =~ s/^\s+//;
+  $input =~ s/\s+$//;
   #
-  # Try to load the good first class immediately
+  # Specific, else generic, else common
   #
   my $new;
   if ($input =~ /^[A-Za-z][A-Za-z0-9+.-]*(?=:)/p) {
-    #
-    # The specific ?
-    #
     my $class = sprintf('%s::%s', __PACKAGE__, ${^MATCH});
-    try { load_class($class); $new = $class->new($input) };
+    try {
+      load_class($class);
+      $new = $class->new($input);
+      $has_recognized_scheme = 1;
+    };
   }
-  #
-  # The generic ?
-  #
   try { $new = MarpaX::RFC::RFC3987::_generic->new($input) } if (! $new);
-  #
-  # At least common must match
-  #
   $new = MarpaX::RFC::RFC3987::_common->new($input) if (! $new);
+  #
+  # scheme argument
+  #
+  if (@_) {
+    my $scheme = shift;
+    if (! $new->can('is_relative')) {
+      warn 'scheme argument ignored: implementation cannot tell if input is relative';
+    } else {
+      if ($new->is_relative) {
+      }
+    }
+  }
 
   $new
 }
+
+sub has_recognized_scheme { $has_recognized_scheme };
 
 1;
--- t/test.pl
+++ t/test.pl
@@ -20,6 +20,6 @@ DEFAULT_LOG4PERL_CONF
 Log::Log4perl::init(\$defaultLog4perlConf);
 Log::Any::Adapter->set('Log4perl');
 
-my $this = MarpaX::RFC::RFC3987->new('UTF8', "http://test?voila1...\&voila2\#f");
+my $this = MarpaX::RFC::RFC3987->new(['UTF8', "http://test?voila1...\&voila2\#f"]);
 
 p $this;

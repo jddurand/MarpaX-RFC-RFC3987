59e819338a31477666cc643957ab0e0bc8806ef9
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-11-04 17:30:54 +0100

    dev+: with order v.s. arounds

--- lib/MarpaX/RFC/RFC3987/_generic.pm
+++ lib/MarpaX/RFC/RFC3987/_generic.pm
@@ -11,8 +11,8 @@ package MarpaX::RFC::RFC3987::_generic;
 use Moo;
 
 with 'MarpaX::RFC::RFC3987::_generic::BNF';
-with 'MarpaX::Role::Parameterized::ResourceIdentifier::Role::_common';
 with 'MarpaX::Role::Parameterized::ResourceIdentifier::Role::_generic';
+with 'MarpaX::Role::Parameterized::ResourceIdentifier::Role::_common';
 
 1;
 

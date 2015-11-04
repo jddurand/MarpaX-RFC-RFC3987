86dca2e0b3f51222a6881db1a8b6c78842388ee0
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-11-04 03:22:25 +0100

    t+

--- t/test.pl
+++ t/test.pl
@@ -26,14 +26,14 @@ Log::Any::Adapter->set('Log4perl');
 
 my $iri = MarpaX::RFC::RFC3987->new(shift
                                     ||
-                                    { input => "HTTp://re\x{301}sume\x{301}.example.org/%7Euser", is_character_normalized => 0, is_reg_name_convert_to_IRI => 'X', is_reg_name_as_domain_name => 1 }
+                                    { octets => "HTTp://www.exAMPLe.org/re+AwE-sume+AwE-/+ACU-7Euser/a/./b/../b/+ACU-63/+ACU-7bfoo+ACU-7d/ros+ACU-C3+ACU-A9/end", encoding => 'UTF-7',
+                                      is_reg_name_convert_to_IRI => 'X', is_reg_name_as_domain_name => 1, is_character_normalized => 0 }
+                                    ||
+                                    { input => "HTTp://re\x{301}sume\x{301}.example.org/%7euser", is_character_normalized => 0, is_reg_name_convert_to_IRI => 'X', is_reg_name_as_domain_name => 1 }
                                     ||
                                     "http://example.com/\x{10300}\x{10301}\x{10302}"
                                     ||
                                     "http://www.example.org/red%09ros\x{E9}#red"
-                                    ||
-                                    { octets => "HTTp://www.exAMPLe.org/re+AwE-sume+AwE-/+ACU-7Euser/a/./b/../b/+ACU-63/+ACU-7bfoo+ACU-7d/ros+ACU-C3+ACU-A9/end", encoding => 'UTF-7',
-                                      is_reg_name_convert_to_IRI => 'X', is_reg_name_as_domain_name => 1 }
                                     ||
                                     { input => "http://r\x{E9}sum\x{E9}.example.org", is_reg_name_convert_to_IRI => 'X', is_reg_name_as_domain_name => 1 }
                                     ||
@@ -51,6 +51,8 @@ my $iri = MarpaX::RFC::RFC3987->new(shift
                                     ||
                                     "/foo/bar"
                                    );
+p($iri);
+exit;
 my $abs = MarpaX::RFC::RFC3987->new('http://a/b/c/d;p?q');
 exit;
 MarpaX::RFC::RFC3987->new_abs('g:h', $abs);

12443d84711d5e252bc48999f6b19da78654fe85
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-11-05 06:08:01 +0100

    t+: BUILDARGS centralization

--- t/test.pl
+++ t/test.pl
@@ -28,12 +28,13 @@ my @overload_test = (MarpaX::RFC::RFC3987->new("http://example.org/~user"), Marp
 print '== test is ' . ($overload_test[0] == $overload_test[1]) ? "OK\n" : "KO\n";
 print '!= test is ' . ($overload_test[0] != $overload_test[1]) ? "OK\n" : "KO\n";
 print '"" test is ' . $overload_test[0] . " and " . $overload_test[1] . "\n";
-exit;
+# exit;
 
 my $iri = MarpaX::RFC::RFC3987->new(shift
                                     ||
-                                    { octets => "HTTp://www.exAMPLe.org:80/re+AwE-sume+AwE-/+ACU-7E+ACU-7euser/a/./b/../b/+ACU-63/+ACU-7bfoo+ACU-7d/ros+ACU-C3+ACU-A9/end", encoding => 'UTF-7',
-                                      is_reg_name_as_domain_name => 1, is_character_normalized => 0, default_port => 80 }
+                                    { octets => "HTTp://www.exAMPLe.org:80/re+AwE-sume+AwE-/+ACU-7E+ACU-7euser/a/./b/../b/+ACU-63/+ACU-7bfoo+ACU-7d/ros+ACU-C3+ACU-A9/end",
+                                      encoding => 'UTF-7',
+                                      is_character_normalized => 0 }
                                     ||
                                     { input => "HTTp://re\x{301}sume\x{301}.example.org/%7euser", is_character_normalized => 1, is_reg_name_convert_to_IRI => 'X', is_reg_name_as_domain_name => 1 }
                                     ||

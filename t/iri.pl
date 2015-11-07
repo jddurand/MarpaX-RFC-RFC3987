6b98d9aa96030fa7392ddb0299a1c4ab0a551574
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-11-07 16:46:16 +0100

    t+: overload and is_absolute tests

--- t/iri.pl
+++ t/iri.pl
@@ -3,6 +3,8 @@ use strict;
 use warnings FATAL => 'all';
 use Scalar::Util qw/blessed/;
 use Test::More;
+binmode STDOUT, ":encoding(utf8)";
+binmode STDERR, ":encoding(utf8)";
 
 our $_test_abs_base;
 BEGIN {
@@ -12,11 +14,16 @@ BEGIN {
 
 subtest "Reference Resolution with base as Str"    => \&_test_abs_base_as_Str;
 subtest "Reference Resolution with base as Object" => \&_test_abs_base_as_Object;
+subtest "Overload with URI compatibility"          => \&_test_overload_with_uri_compatibility;
+subtest "Overload without URI compatibility"       => \&_test_overload_without_uri_compatibility;
+subtest "is_absolute"                              => \&_test_is_absolute;
 
 done_testing();
 
-sub _test_abs_base_as_Str    { _test_abs($_test_abs_base) }
-sub _test_abs_base_as_Object { _test_abs(MarpaX::RFC::RFC3987->new($_test_abs_base)) }
+sub _test_abs_base_as_Str                    { _test_abs($_test_abs_base) }
+sub _test_abs_base_as_Object                 { _test_abs(MarpaX::RFC::RFC3987->new($_test_abs_base)) }
+sub _test_overload_with_uri_compatibility    { _test_overload(1) }
+sub _test_overload_without_uri_compatibility { _test_overload(0) }
 
 sub _test_abs {
   my $base = shift;
@@ -76,5 +83,46 @@ sub _test_abs {
     my $wanted = $ref2base{$_};
     my $got    = MarpaX::RFC::RFC3987->new($_)->abs($base);
     is($got, $wanted, "base '$base' as $blessed_base, ref '$_'");
+  }
+}
+
+sub _test_overload {
+  my $uri_compat = shift;
+
+  plan tests => 2;
+  local $ENV{MarpaX_RI_URI_COMPAT} = $uri_compat;
+
+  my @overload_test = (MarpaX::RFC::RFC3987->new("http://example.org/~user"),
+                       MarpaX::RFC::RFC3987->new("http://example.org/%7euser"));
+  if ($uri_compat) {
+    ok($overload_test[0] != $overload_test[1], "'$overload_test[0]' != '$overload_test[1]'");
+    ok($overload_test[0] ne $overload_test[1], "'$overload_test[0]' ne '$overload_test[1]'");
+  } else {
+    ok($overload_test[0] == $overload_test[1], "'$overload_test[0]' == '$overload_test[1]'");
+    ok($overload_test[0] ne $overload_test[1], "'$overload_test[0]' ne '$overload_test[1]'");
+  }
+}
+
+sub _test_is_absolute {
+  my @URI = (
+             [ "/localhost/?jdd#f±f2"                                => 0 ],
+             [ "ftp://ftp.is.co.za/rfc/rfc1808.txt"                  => 1 ],
+             [ "http://www.ietf.org/rfc/rfc2396.txt"                 => 1 ],
+             [ "ldap://[2001:db8::7]/c=GB?objectClass?one"           => 1 ],
+             [ "mailto:John.Doe\@example.com"                        => 1 ],
+             [ "news:comp.infosystems.www.servers.unix"              => 1 ],
+             [ "tel:+1-816-555-1212"                                 => 1 ],
+             [ "telnet://192.0.2.16:80/"                             => 1 ],
+             [ "urn:oasis:names:specification:docbook:dtd:xml:4.1.2" => 1 ],
+             [ "foo://example.com:8042/over/there?name=ferret#nose"  => 1 ],
+             [ "example.com:8042/over/there?name=ferret#nose"        => 1 ],   # example.com looks like a scheme
+             [ "urn:example:animal:ferret:nose"                      => 1 ],
+             [ "http://\x{7D0D}\x{8C46}.example.org/%E2%80%AE"       => 1 ],
+            );
+  plan tests => scalar(@URI);
+
+  foreach (@URI) {
+    my $is_absolute = MarpaX::RFC::RFC3987->new($_->[0])->is_absolute;
+    ok($_->[1] ? $is_absolute : ! $is_absolute, "'$_->[0]' is " . ($_->[1] ? '' : 'not ') . 'absolute');
   }
 }

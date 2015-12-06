dc503b8a023970090def7fd25f5e9ce96718f17c
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-12-06 08:33:08 +0100

    https plugin extends http bnf

--- lib/MarpaX/RFC/RFC3987/Impl/Plugins/https.pm
+++ lib/MarpaX/RFC/RFC3987/Impl/Plugins/https.pm
@@ -2,7 +2,6 @@ use strict;
 use warnings FATAL => 'all';
 
 package MarpaX::RFC::RFC3987::Impl::Plugins::https;
-use Moo;
 
 # ABSTRACT: Internationalized Resource Identifier (IRI): https implementation
 
@@ -10,12 +9,9 @@ use Moo;
 
 # AUTHORITY
 
-use Carp qw/croak/;
-use File::ShareDir::ProjectDistDir 1.0 qw/dist_dir/, strict => 1;
-use File::Spec qw//;
-use IO::File qw//;
-use MarpaX::RFC::RFC3987::Impl::_generic;
 use Moo;
+use MarpaX::RFC::RFC3987::Impl::Plugins::http;
+
 #
 # Scheme registration
 #
@@ -23,30 +19,10 @@ sub can_scheme { my ($class, $scheme) = @_; $scheme =~ /\Ahttps\z/i }
 
 our ($ROLE_PARAMS);
 BEGIN {
-  # ---
-  # BNF: https BNF "inherits" from _generic BNF
-  # ---
-  my $bnf_file   = File::Spec->catfile(dist_dir('MarpaX-RFC-RFC3987'), 'https.bnf');
-  my $fh         = IO::File->new($bnf_file, 'r');
-  my $BNF        = do { local $/; <$fh> };
-  $bnf_file      = File::Spec->catfile(dist_dir('MarpaX-RFC-RFC3987'), '_generic.bnf');
-  $fh            = IO::File->new($bnf_file, 'r');
-  $BNF          .= do { local $/; <$fh> };
-  $BNF =~ /\A(.*)\z/s || croak 'Failed to untaint';
-  $BNF = $1; # We trust our BNF
-  # ---------
-  # For reuse
-  # ---------
-  $ROLE_PARAMS = MarpaX::RFC::RFC3987::Impl::_generic->role_params_clone;
-  #
-  # Overwrite/amend our specifics
-  #
-  $ROLE_PARAMS->{whoami}      = __PACKAGE__;
-  $ROLE_PARAMS->{extends}     = 'MarpaX::RFC::RFC3987::Impl::_generic';
-  $ROLE_PARAMS->{start}       = '<https>';
-  $ROLE_PARAMS->{bnf}         = $BNF;
-  $ROLE_PARAMS->{server}      = 1;
-  $ROLE_PARAMS->{userpass}    = 1;
+  $ROLE_PARAMS = MarpaX::RFC::RFC3987::Impl::Plugins::http->role_params_clone;
+  $ROLE_PARAMS->{whoami}  = __PACKAGE__;
+  $ROLE_PARAMS->{start}   = '<https>';
+  $ROLE_PARAMS->{bnf}     =~ s/\bhttp\b/https/g;
 }
 
 sub role_params { $ROLE_PARAMS }

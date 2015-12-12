bf797b8b627551ec5bcbcec4e33d7f58b9e225ae
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-12-12 07:59:21 +0100

    ldaps

---
+++ lib/MarpaX/RFC/RFC3987/Impl/Plugins/ldaps.pm
@@ -0,0 +1,36 @@
+use strict;
+use warnings FATAL => 'all';
+
+package MarpaX::RFC::RFC3987::Impl::Plugins::ldaps;
+
+# ABSTRACT: Internationalized Resource Identifier (IRI): ldaps implementation
+
+# VERSION
+
+# AUTHORITY
+
+use Moo;
+use MarpaX::RFC::RFC3987::Impl::Plugins::ldap;
+
+#
+# Scheme registration
+#
+sub can_scheme { my ($class, $scheme) = @_; $scheme =~ /\Aldaps\z/i }
+
+our ($ROLE_PARAMS);
+BEGIN {
+  $ROLE_PARAMS = MarpaX::RFC::RFC3987::Impl::Plugins::ldap->role_params_clone;
+  $ROLE_PARAMS->{whoami}  = __PACKAGE__;
+  $ROLE_PARAMS->{start}   = '<ldapsurl>';
+  $ROLE_PARAMS->{bnf}     =~ s/'ldap'/'ldaps'/g;
+  $ROLE_PARAMS->{bnf}     =~ s/\bldapurl\b/ldapsurl/g;
+}
+
+sub role_params { $ROLE_PARAMS }
+
+use MooX::Role::Parameterized::With 'MarpaX::Role::Parameterized::ResourceIdentifier::BNF' => $ROLE_PARAMS;
+
+with 'MarpaX::RFC::RFC3987::Role::Plugins::ldaps';
+with 'MarpaX::Role::Parameterized::ResourceIdentifier::Role::ldaps';
+
+1;
---
+++ lib/MarpaX/RFC/RFC3987/Role/Plugins/ldaps.pm
@@ -0,0 +1,16 @@
+use strict;
+use warnings FATAL => 'all';
+
+package MarpaX::RFC::RFC3987::Role::Plugins::ldaps;
+use Moo::Role;
+
+# ABSTRACT: Internationalized Resource Identifier (IRI): ldaps plugin role
+
+# VERSION
+
+# AUTHORITY
+
+with 'MarpaX::RFC::RFC3987::Role::_generic';
+
+1;
+
